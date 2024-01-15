51655b69a5cd42fbb2b981701fc4e8eea0c360b7
Divija Durga <107680271+Dragonsarecool123@users.noreply.github.com>
2024-01-15 22:27:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+let active = false
 
+
+function toggle() {
+    let toggle = document.querySelector('.toggle')
+    let text = document.querySelector('.text')
+    active = !active
+    if (active) {
+        toggle.classList.add('active')
+      visitPage()
+       setTimeout(function() {
+        toggle.classList.remove('active')      }, 1000);
+      
+    } 
+}
 
   function visitPage(){
-    window.location='https://blr1.blynk.cloud/external/api/update?token=3p0B4uIgPAUfEmcEeAxx1785et4jwxcc&V0=1';
+    window.open('https://blr1.blynk.cloud/external/api/update?token=3p0B4uIgPAUfEmcEeAxx1785et4jwxcc&V0=1', 'popUpWindow','height=400,width=400,left=10,top=10,,scrollbars=yes,menubar=no') 
+    return false
 }
