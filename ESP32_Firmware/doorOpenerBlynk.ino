484823604ed1bc3811b66d06e84a45e86e39ada0
Divija Durga <107680271+dividurga@users.noreply.github.com>
2024-01-25 23:14:01 +0530

    formatted and fixed bug causing esp32 to go offline

--- ESP32_Firmware/doorOpenerBlynk.ino
+++ ESP32_Firmware/doorOpenerBlynk.ino
@@ -1,12 +1,12 @@
 /*************************************************************
 
-Firmware for triggering a servo evertime the API request is sent
-*************************************************************/
+  This is a simple demo of sending and receiving some data.
+ *************************************************************/
 
 /* Fill-in information from Blynk Device Info here */
-#define BLYNK_TEMPLATE_ID "Redacted"
-#define BLYNK_TEMPLATE_NAME "Redacted"
-#define BLYNK_AUTH_TOKEN "Redacted"          
+#define BLYNK_TEMPLATE_ID "TMPL39IPuuPfy"
+#define BLYNK_TEMPLATE_NAME "doorOpener"
+#define BLYNK_AUTH_TOKEN "3p0B4uIgPAUfEmcEeAxx1785et4jwxcc"
 
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
@@ -20,63 +20,58 @@ Servo servo;
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "redacted";
-char pass[] = "redacted";
+char ssid[] = "bigbang";
+char pass[] = "divij@123";
 
 BlynkTimer timer;
 
-void wait(int delay_value){
+void wait(int delay_value) {
   //this function is used instead of the delay function since blynk recommends millis()
-  unsigned long starttime=millis();
-  while(millis()-starttime<delay_value){
+  unsigned long starttime = millis();
+  while (millis() - starttime < delay_value) {
     //do nothing
     //used instead of delay so that connection is not severed
-
   }
-
 }
 // This function is called every time the Virtual Pin 0 state changes
-BLYNK_WRITE(V0)
-{
+BLYNK_WRITE(V0) {
   // Set incoming value from pin V0 to a variable
   int value = param.asInt();
-  if (value==1){
-    for(int j=0; j<=180; j++){
+  if (value == 1) {
+    for (int j = 0; j <= 180; j++) {
       servo.write(j);
       wait(3);
     }
-wait(1000);
-    }
-    for(int j=180; j>=0; j--){
-      servo.write(j);
-      wait(3);
-    value=0;
-}
-    
-  }
+    wait(1000);
 
+    for (int j = 180; j >= 0; j--) {
+      servo.write(j);
+      wait(3);
+    }
+    value = 0;
+  }
+}
 
 
 // This function is called every time the device is connected to the Blynk.Cloud
 
 // This function sends Arduino's uptime every second to Virtual Pin 2.
 
-void myTimerEvent()
-{
+void myTimerEvent() {
   // You can send any value at any time.
   // Please don't send more that 10 values per second.
 }
 
-void setup()
-{
+void setup() {
   // Debug console
   Serial.begin(115200);
   ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
-	servo.setPeriodHertz(50);    // standard 50 hz servo
-	servo.attach(servoPin, 500, 2400);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  servo.setPeriodHertz(50);  // standard 50 hz servo
+  servo.attach(servoPin, 500, 2400);
+  pinMode(15, OUTPUT);
 
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
   // You can also specify server:
@@ -87,8 +82,7 @@ void setup()
   timer.setInterval(1000L, myTimerEvent);
 }
 
-void loop()
-{
+void loop() {
   Blynk.run();
   timer.run();
   // You can inject your own code or combine it with other sketches.
